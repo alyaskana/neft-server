079865df4356f63e451f5dcdd48f6b5d29546056
Alina Bychkova <ialina240200@gmail.com>
2023-03-08 22:24:49 +0100

    Update plot after the seed has grown

--- app/jobs/grow_seed_job.rb
+++ app/jobs/grow_seed_job.rb
@@ -1,7 +1,18 @@
 class GrowSeedJob < ApplicationJob
+  include ChannelHelper
   queue_as :default
 
-  def perform(grownig_seed)
-    grownig_seed.update!(stage: 'complete')
+  def perform(growing_seed)
+    growing_seed.update!(stage: 'complete')
+    user = growing_seed.user
+    message = {
+      type: 'updatePlots',
+      data: render_json(
+        template: 'api/games/plots',
+        locals: { plots: user.plots }
+      )
+    }
+
+    GameChannel.broadcast_to(user, message)
   end
 end
