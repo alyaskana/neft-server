3b52bbca0c8b5e57c6c714de266115637ee64244
Alina Bychkova <ialina240200@gmail.com>
2023-04-08 15:20:13 +0200

    Add fish level calculation

--- app/services/games/eat_crop.rb
+++ app/services/games/eat_crop.rb
@@ -3,7 +3,8 @@ module Games
     def call(user, crop_id, fish_id)
       crop = Crop.find_by(user: user, id: crop_id)
       fish = Fish.find_by(user: user, id: fish_id)
-      fish.update!(experience: fish.experience + crop.plant.experience)
+      xp = fish.experience + crop.plant.experience
+      fish.update!(experience: xp, level: (0.07 * Math.sqrt(xp) + 1).to_i)
       crop.update!(count: crop.count - 1)
     end
   end
