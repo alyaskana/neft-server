cf116390df4b209a2cbc919c2d1ca09cff55b18b
Alina Bychkova <ialina240200@gmail.com>
2023-06-02 20:30:41 +0200

    Fix

--- app/services/games/cook_recipe.rb
+++ app/services/games/cook_recipe.rb
@@ -18,7 +18,7 @@ module Games
       final_cook_time = Time.now + user_recipe.recipe.cooking_time
       call_job_time = Time.now + user_recipe.recipe.cooking_time / 20 - 2
       user_recipe.update!(stage: 'cooking', final_cook_time: final_cook_time)
-      dish = Dish.find_by(user: user, recipe: user_recipe.recipe)
+      dish = Dish.find_or_create_by!(user: user, recipe: user_recipe.recipe)
       CookRecipeJob.set(wait_until: call_job_time).perform_later(dish, user_recipe)
     end
   end
